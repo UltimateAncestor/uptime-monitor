1e34520369046d85da1f3414189e4ed231860405
deniskorovko <dkcreator@gmail.com>
2026-07-12 22:11:17 +0300

    Initial commit

---
+++ src/main/java/ru/dk/uptimemonitoring/UptimeMonitoringApplication.java
@@ -0,0 +1,13 @@
+package ru.dk.uptimemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimeMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dk/uptimemonitoring/UptimeMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dk.uptimemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
